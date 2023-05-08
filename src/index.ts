dcf161b03b5feaa552d1fc4597dfa132582e7f23
Vladislav Suchkov <silikovaniy@mail.ru>
2023-05-09 07:30:40 +1000

    added second console.log to example

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 export function sayHello() {
     debugLog('saying hello...');
-    debugLog('and saying hello...');
     debugLog('foo');
     debugLog('bar');
     debugLog('foo');
 
     console.log('hello world!');
+
+    debugLog('and saying hello again...');
+    debugLog('faz');
+    debugLog('faz');
+    console.log('hello world, again!');
 }
